12f8f8d1e7c96be0f38fd127c515b3a6127c9498
Kevin Jiao <kevin.jiao@berkeley.edu>
2015-09-20 03:48:34 -0700

    add reset

--- server.py
+++ server.py
@@ -15,6 +15,19 @@ from flask import Flask, Response, request
 app = Flask(__name__, static_url_path='', static_folder='')
 app.add_url_rule('/', 'root', lambda: app.send_static_file('index.html'))
 
+@app.route('/reset', methods=['GET', 'POST'])
+def comments_handler():
+
+    if request.method == 'POST':
+
+        with open('data.json', 'r') as r:
+            data = json.load(f)
+            data["game_state"]="reset"
+
+        with open('data.json', 'w') as f:
+            f.write(json.dumps(data));
+
+    return Response(json.dumps(data), mimetype='application/json', headers={'Cache-Control': 'no-cache'})
 
 if __name__ == '__main__':
     app.run(port=int(os.environ.get("PORT",3000)))
